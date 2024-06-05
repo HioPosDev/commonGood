fbd10146f256fd78856e7c0a80e9d9719e255bd2
Pere <pripoll91@gmail.com>
2024-06-05 20:45:47 +0200

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const arrayValues = [
+    {
+      name: 'primer plato',
+      state: '1'
+    },
+    {
+      name: 'segundo plato',
+      state: '1'
+    },
+    {
+      name: 'postre',
+      state: '1'
+    },
+    {
+      name: 'ron cola',
+      state: '1'
+    }
+  ]
 
   return (
-    <>
+    <div className='app-container'>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,17 +35,19 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {arrayValues.map(({name, state}) => {
+          return (
+            <div className='article'>
+              <p>Name: {name}</p>
+              <p>State: {state}</p>
+            </div>
+          )
+        })}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
