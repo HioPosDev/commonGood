494ee8fb3e797f6e99addb2e0a9a979d46dde3c9
Pere <pripoll91@gmail.com>
2024-06-13 19:44:36 +0200

    test

--- src/components/notification-component/product-section.component.tsx
+++ src/components/notification-component/product-section.component.tsx
@@ -1,36 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const ProductSection = () => {
   const [vibracionActiva, setVibracionActiva] = useState(false);
+  const [productsReady, setProductsReady] = useState(false);
 
   // Función para simular la recepción de estado "ready" desde el backend
-  const simularEstadoReady = () => {
-    if (vibracionActiva) {
-      navigator.vibrate([1000]); // Activar vibración cuando un producto está listo
+  useEffect(() => {
+    if (vibracionActiva && productsReady) {
+      navigator.vibrate([800, 200, 800]) // Activar vibración cuando un producto está listo
 
-      // Mostrar notificación usando react-toastify
-      toast.success('¡Producto listo!', { autoClose: 3000 });
-    }
-  };
+    // Mostrar notificación usando react-toastify
+    toast.success('¡Producto listo!', { autoClose: 3000 });
+  }
+  }, [productsReady, vibracionActiva])
 
   const handleAccederProductos = () => {
-    navigator.vibrate([800, 200, 800]); // Iniciar la vibración al acceder al apartado de productos
+    // navigator.vibrate([800, 200, 800]); // Iniciar la vibración al acceder al apartado de productos
+    setTimeout(() => setProductsReady(true), 1000);
     setVibracionActiva(true);
-    mostrarProductos(); // Función para mostrar productos
   };
 
-  const mostrarProductos = () => {
-    // Implementa la lógica para mostrar productos
-  };
 
   return (
     <div>
       <h2>Sección de Productos</h2>
       <button onClick={handleAccederProductos}>Acceder a Productos</button>
       <br />
-      <button onClick={simularEstadoReady}>Simular Producto Ready</button>
+      {/* <button onClick={simularEstadoReady}>Simular Producto Ready</button> */}
       <ToastContainer /> {/* Componente de react-toastify para mostrar las notificaciones */}
     </div>
   );
