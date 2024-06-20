302a475ff7cfba6bb5a277fe58fa03a0bfd4f5b7
Pere <pripoll91@gmail.com>
2024-06-20 20:00:47 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/main.tsx',  // Ajusta la ruta según donde esté tu punto de entrada principal
+        sw: './sw.js',           // Ruta al archivo sw.js
+      },
+      output: {
+        entryFileNames: '[name].[hash].js',    // Nombre del archivo de entrada
+        chunkFileNames: '[name].[hash].js',    // Nombre de archivos chunk
+        assetFileNames: '[name].[hash].[ext]', // Nombre de archivos de assets
+      },
+    },
+  },
+});
