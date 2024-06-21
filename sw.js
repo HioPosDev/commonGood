2cd6c3c0b6f9446928b53014f64af56e824600ed
Pere <pripoll91@gmail.com>
2024-06-21 12:53:58 +0200

    test

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener('push', event => {
     const options = {
       body: data.body,
       icon: 'icon.png',
-      vibrate: [2000, 500, 2000] // Patron de vibración (milisegundos)
+      vibrate: [2000, 500, 2000, 3000, 100, 1000, 300, 1000] // Patron de vibración (milisegundos)
     };
   
     event.waitUntil(
