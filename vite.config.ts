6a095193949565fafa51d6fce8412a0218483530
Pere <pripoll91@gmail.com>
2024-06-21 12:02:55 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './src/main.tsx',  // Ajusta la ruta según donde esté tu punto de entrada principal
-        sw: './sw.js',           // Ruta al archivo sw.js
-      },
-      output: {
-        entryFileNames: '[name].[hash].js',    // Nombre del archivo de entrada
-        chunkFileNames: '[name].[hash].js',    // Nombre de archivos chunk
-        assetFileNames: '[name].[hash].[ext]', // Nombre de archivos de assets
-      },
-    },
-  },
-});
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'sw.js', // El archivo que quieres copiar
+          dest: '' // La carpeta de destino (en este caso, la raíz de 'dist')
+        }
+      ]
+    })
+  ]
+})
