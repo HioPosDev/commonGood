fd561ecbd05ee4098f629b67da88fb1da316addb
Pere <pripoll91@gmail.com>
2024-07-16 13:07:01 +0200

    new pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {GeneralProvider} from './context/generalContext.tsx'
+import { registerServiceWorker } from './serviceWorkerRegistration.ts';
 
+registerServiceWorker();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GeneralProvider>
---
+++ src/serviceWorkerRegistration.ts
@@ -0,0 +1,16 @@
+// src/serviceWorkerRegistration.js
+
+export const registerServiceWorker = () => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+          .then(registration => {
+            console.log('Service Worker registrado:', registration);
+          })
+          .catch(error => {
+            console.error('Error al registrar el Service Worker:', error);
+          });
+      });
+    }
+  };
+  
